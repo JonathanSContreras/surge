fecea241579a07981a1ad55a34e3202e696ef8e0
Brianna Hinds <briden32016@gmail.com>
2025-08-21 13:16:25 -0500

    building SAM

--- model/sam.py
+++ model/sam.py
@@ -1,1 +1,44 @@
-# LangChain agent using gpt-oss
+# LangChain (v0.3.74) agent using gpt-oss or claude
+
+# libraries
+from langchain.memory import ConversationBufferMemory
+from langchain.agents import initialize_agent
+from toolkit import *
+from ..src.xml_to_network import dictionary_to_networkx
+
+
+LLM = 0 # DEFINE LLM API HERE
+
+# define memory
+memory = ConversationBufferMemory(memory_key="history", return_messages=True)
+
+# define the toolkit 
+tools = [ping_sweep, 
+         port_scan_stealth, 
+         port_scan_stealth, 
+         port_scan_decoy, 
+         port_scan_aggressive, 
+         service_enum, 
+         os_fingerprint, 
+         vuln_scan, 
+         pseudo_exploit]
+
+system_prompt = """"""
+llm = # DEFINE LLM API HERE
+
+
+# create an Agent instance
+sam = initialize_agent(
+    tools=tools,
+    llm=llm,
+    system_prompt=
+)
+
+# run SAM
+"""
+1. takes the goal
+2. SAM decides sequence of tools to run
+3. each tool result is parsed
+"""
+def run_sam():
+    pass
