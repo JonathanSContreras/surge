976aba97f091da7a5f2a1f202105745a14190540
Brianna Hinds <briden32016@gmail.com>
2025-10-20 11:47:35 -0500

    running recon and recon analysis

--- model/sam_oss.py
+++ model/sam_oss.py
@@ -380,7 +380,7 @@ sam = workflow.compile()
 if __name__ == "__main__":
     initial_state = {
         "scan_type": "high",
-        "targets": ["255.255.0.0/16"],  # whole subnet scan  ["10.10.162.0/24"]
+        "targets": ["10.10.0.0/16"],  # whole subnet scan  ["10.10.162.0/24"]
         "recon_results": {},
         "vuln_results": [],
         "network_findings": ""
